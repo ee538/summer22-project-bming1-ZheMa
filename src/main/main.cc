7742ee7e521b9e4ef8cca07e1072be53494957e2
J.Ma <m.ma@JMas-MBP-4.lan>
2022-07-22 12:55:03 -0700

    phase2_3_tmp3

--- src/main/main.cc
+++ src/main/main.cc
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "src/lib/trojanmap.h"
 #include "src/lib/mapui.h"
-using namespace std;
 // #define NCURSES
 
-/*int main() {
+int main() {
   MapUI x;
   #ifdef NCURSES
     x.PlotMap();
@@ -13,21 +12,5 @@ using namespace std;
     x.PlotMap();
     x.PrintMenu();
   #endif
-  return 0;
-}*/
-
-int main() {
-  TrojanMap m;
-  std::vector<std::string> Categories1;
-  //m.CreateGraphFromCSVFile();
-
-  auto result = m.FindClosestName("Rophs");
-  //  Categories1 = m.GetAllCategories();
-  //  for (auto &i : Categories1 )
-  //  {
-  //    std::cout << "Value of str is : " << i << std::endl;
-  //  }
-  
-
   return 0;
 }
